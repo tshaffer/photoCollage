f58be05bb1f651aeabb3b24747ec310be29cf847
Ted Shaffer <ted@brightsign.biz>
2020-12-20 06:36:13 -0800

    first round of merging material ui dialog example with photo collage code.

--- src/component/PhotoCollage.tsx
+++ src/component/PhotoCollage.tsx
@@ -143,59 +143,219 @@
 
 // const PhotoCollage = (): any => {
 
-import React from 'react';
+// import React from 'react';
+// import Button from '@material-ui/core/Button';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+
+// const PhotoCollage = (): any => {
+
+//   const [open, setOpen] = React.useState(false);
+
+//   const handleClickOpen = () => {
+//     setOpen(true);
+//   };
+
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+
+//   return (
+//     <div>
+//       <Button variant='outlined' color='primary' onClick={handleClickOpen}>
+//         Open alert dialog
+//       </Button>
+//       <Dialog
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby='alert-dialog-title'
+//         aria-describedby='alert-dialog-description'
+//       >
+//         <DialogTitle id='alert-dialog-title'>{'Use Google's location service?'}</DialogTitle>
+//         <DialogContent>
+//           <DialogContentText id='alert-dialog-description'>
+//             Let Google help apps determine location. This means sending anonymous location data to
+//             Google, even when no apps are running.
+//           </DialogContentText>
+//           <DialogContentText id='second-text-field'>
+//             Pizza is my favorite food.
+//           </DialogContentText>
+//         </DialogContent>
+//         <DialogActions>
+//           <Button onClick={handleClose} color='primary'>
+//             Disagree
+//           </Button>
+//           <Button onClick={handleClose} color='primary' autoFocus>
+//             Agree
+//           </Button>
+//         </DialogActions>
+//       </Dialog>
+//     </div>
+//   );
+// }
+
+// export default PhotoCollage;
+
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Modal from 'react-modal';
+
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-const PhotoCollage = (): any => {
+import { blue } from '@material-ui/core/colors';
 
-  const [open, setOpen] = React.useState(false);
+import { isNil } from 'lodash';
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+import {
+  PhotoCollageState,
+  DisplayedPhoto,
+} from '../type';
+import PhotoCollageCanvas from './PhotoCollageCanvas';
+
+import {
+  startPlayback,
+} from '../controller';
+
+export interface PhotoCollageComponentState {
+  showModal: boolean;
+  selectedPhoto: DisplayedPhoto | null;
+}
+
+// -----------------------------------------------------------------------
+// Types
+// -----------------------------------------------------------------------
+
+/** @internal */
+/** @private */
+export interface PhotoCollageProps {
+  onStartPlayback: () => any;
+}
+
+// -----------------------------------------------------------------------
+// Component
+// -----------------------------------------------------------------------
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600],
+  },
+});
+
+interface PhotoDialogProps {
+  open: boolean;
+  selectedValue: DisplayedPhoto;
+  onClose: (value: DisplayedPhoto) => void;
+}
+
+const PhotoDialog = (props: PhotoDialogProps) => {
+  const classes = useStyles();
+  const { onClose, selectedValue, open } = props;
 
   const handleClose = () => {
-    setOpen(false);
+    onClose(selectedValue);
   };
 
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
       <Dialog
         open={open}
         onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
       >
-        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogTitle id='alert-dialog-title'>{'Eat pizza?'}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
+          <DialogContentText id='alert-dialog-description'>
             Let Google help apps determine location. This means sending anonymous location data to
             Google, even when no apps are running.
           </DialogContentText>
-          <DialogContentText id="second-text-field">
-            Pizza is my favorite food.
+          <DialogContentText id='eat pizza - description'>
+            Eat more pizza
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handleClose} color='primary'>
             Disagree
           </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
+          <Button onClick={handleClose} color='primary' autoFocus>
             Agree
           </Button>
         </DialogActions>
       </Dialog>
     </div>
-  );
+  )
 }
 
+const PhotoCollage = (props: PhotoCollageProps) => {
 
-export default PhotoCollage;
+  const [open, setOpen] = React.useState(false);
+
+  const [_selectedPhoto, setSelectedPhoto] = React.useState<DisplayedPhoto | undefined>(undefined);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSelectPhoto = (selectedPhoto: DisplayedPhoto) => {
+    setSelectedPhoto(selectedPhoto);
+    setOpen(true);
+  };
+
+  const renderDialog = () => {
+    // if (isNil(_selectedPhoto)) {
+    //   return (
+    //     <div>
+    //       <button onClick={handleCloseModal}>Close Modal</button>
+    //     </div>
+    //   );
+    // }
+    const selectedPhoto: DisplayedPhoto | undefined = _selectedPhoto;
+
+    if (isNil(selectedPhoto)) {
+      return null;
+    }
+    return (
+      <PhotoDialog
+        selectedValue={selectedPhoto}
+        open={open}
+        onClose={handleClose}
+      />
+    )
+
+  };
+
+  return (
+    <div>
+      <PhotoCollageCanvas
+        onSelectPhoto={handleSelectPhoto}
+      />
+      {renderDialog()}
+      pizza
+    </div >
+  );
+
+};
+
+function mapStateToProps(state: PhotoCollageState, ownProps: any) {
+  return {
+  };
+}
+
+const mapDispatchToProps = (dispatch: any) => {
+  return bindActionCreators({
+    onStartPlayback: startPlayback,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhotoCollage);
